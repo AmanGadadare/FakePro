574806df6520dcde0146b536aa155aff29d83a69
Aman Gadadare <93522857+AmanGadadare@users.noreply.github.com>
2025-04-06 13:41:28 +0530

    Update supplychain.sql

--- sql/supplychain.sql
+++ sql/supplychain.sql
@@ -10,9 +10,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 INSERT INTO `users` (`id`, `email`, `username`, `password`, `role`) VALUES
-(5, 'anubhavduttas@hotmail.com', 'Anubhav Dsutta', '0cc175b9c0f1b6a831c399e269772661', 0),
 (6, 'samplemanufacturer@gmail.com', 'SampleManufacturer', '5f4dcc3b5aa765d61d8327deb882cf99', 0),
 (7, 'samplelogistics@gmail.com', 'SampleLogistics', '5f4dcc3b5aa765d61d8327deb882cf99', 1),
 (8, 'samplecustomer@gmail.com', 'SampleCustomer', '5f4dcc3b5aa765d61d8327deb882cf99', 2);
 
-# Developed by Anubhav Dutta : https://www.linkedin.com/in/iamanubhavdutta/
